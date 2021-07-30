77ca888e094f234c416e2b11fda38af3da8010a7
gilceleno sena <gilcelenoxavierdesena>
2021-07-30 11:00:40 -0300

    Trabalhando com variaveis, laços de repetiçoes, datas, alert, console.log

---
+++ js/java.js
@@ -0,0 +1,46 @@
+/*
+var nome = "gilceleno";
+var idade = 23;
+var idade2 = 45;
+alert (nome + " tem" + idade + "anos");
+console.log(nome);
+console.log(idade + idade2);
+*/
+/*var lista= ["maça","pera","laranja"];
+console.log(lista[1]);
+console.log(lista[1]);
+//lista.push("uva");
+console.log(lista);
+//lista.pop("uva");
+console.log(lista);
+//console.log(lista.toString);
+console.log(lista.join('-'));
+*/
+
+/*var frutas = [{nome:"maçã", cor:"verde"}, {nome:"morango", cor:"vermelha"}]; 
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var idade = prompt("Qual a sua idade");
+if (idade >= 18){
+    alert("Maior de idade");
+}
+else{
+    alert("menor de idade");
+}
+*/
+
+/*
+var count= 0;
+while (count <= 5){
+    console.log(count);
+    count ++;
+}
+*/
+
+
+for(count=1; count<=6; count++){
+    alert(count);
+}
+
